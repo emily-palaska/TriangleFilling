ef647ff64349cd739bfc436f8e5cec1c73750d19
Emily Palaska <103355365+emily-palaska@users.noreply.github.com>
2024-04-06 18:33:34 +0300

    converted to numpy

--- g_shading.py
+++ g_shading.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 import random
 from vector_interp import vector_interp
@@ -33,7 +34,7 @@ def bresenham_line(start, end):
         if e2 < dx:
             err += dx
             y1 += sy
-    return pixels
+    return np.array(pixels)
 
 # Function that fills a given triangle with the Gouraud shading technique
 # Given that the algorithm only handles triangles, non-convex polygons are not considered in the implentation
@@ -47,46 +48,33 @@ def bresenham_line(start, end):
 #   update_img: the new canvas with the filled triangle
 def g_shading(img, vertices, vcolors):    
     # Calculate the y scanning range
-    ymin = min(vertices[0][1], vertices[1][1], vertices[2][1])
-    ymax = max(vertices[0][1], vertices[1][1], vertices[2][1])
-
+    ymin = np.min(vertices[:, 1])
+    ymax = np.max(vertices[:, 1])
+    
     # Find the edges of the triangle using the Bresenham Algorithm on every combination of vertices
-    active_edges = []
-    for i in range(3):
-        start = vertices[i % 3]
-        end = vertices[(i + 1) % 3]
-        active_edges += bresenham_line(start, end)
-    
-    # Sort the vertices and edges firstly by y and then by x
-    active_edges = sorted(active_edges, key=lambda coord: (coord[1], coord[0]))
-    sorted_vertices = sorted(vertices, key=lambda coord: (coord[1], coord[0]))
-    
-    # Get the indices of sorted vertices in the original list
-    indices = [vertices.index(vertex) for vertex in sorted_vertices]    
-   
-    # Use the indices to sort vcolors
-    sorted_vcolors = [vcolors[index] for index in indices]
+    active_edges = bresenham_line(vertices[0, :], vertices[1, :])
+    active_edges = np.concatenate([active_edges, bresenham_line(vertices[1, :], vertices[2,:])])
+    active_edges = np.concatenate([active_edges, bresenham_line(vertices[2, :], vertices[0,:])])
 
-    # Copy the sorted lists back to the original for ease of use
-    vertices = sorted_vertices
-    vcolors = sorted_vcolors
+    # Sort the vertices and edges firstly by y and then by x
+    active_edges = active_edges[np.lexsort((active_edges[:, 0], active_edges[:, 1]))]
+    sorted_vertices_indexes = np.lexsort((vertices[:, 0], vertices[:, 1]))
+    vertices = vertices[sorted_vertices_indexes]
+    vcolors = vcolors[sorted_vertices_indexes]
     
     # Initialize the result image and border colors for each y line
-    updated_img = img
-    border_colors = [[0.0 for i in range(3)] for j in range(2)]
+    updated_img = img.copy()
+    border_colors = np.zeros((2, 3))
     
     # Color in the vertices
     for i in range(3):
         [x, y] = vertices[i]
-        updated_img[x][y] = vcolors[i]   
+        updated_img[x, y] = vcolors[i]   
     
     # Scan all the y lines in the calculated range
     for y in range(ymin, ymax, 1):
         # Move all the points with the same y into the current edges list
-        current_edges = []
-        while active_edges[0][1] == y:
-            x, _ = active_edges.pop(0)
-            current_edges.append(x)
+        current_edges = active_edges[active_edges[:, 1] == y][:, 0]
         
         # Skip the lines with only one vertex point
         if len(current_edges) <= 1:
@@ -94,41 +82,35 @@ def g_shading(img, vertices, vcolors):
         
         # Find the two border clors of the scanning line (left on position 0 and right on position 1)
         order = [0, 1] if vertices[1][0] < vertices[2][0] else [1, 0]
-        for c in range(3):
-            # Decide the points to interpolate based on y
-            if y < vertices[1][1]:
-                border_colors[order[0]][c] = vector_interp(vertices[0], vertices[1], vcolors[0][c], vcolors[1][c], y, 2)
-            else:
-                border_colors[order[0]][c] = vector_interp(vertices[1], vertices[2], vcolors[1][c], vcolors[2][c], y, 2)            
-            border_colors[order[1]][c] = vector_interp(vertices[0], vertices[2], vcolors[0][c], vcolors[2][c], y, 2)        
+        
+        # Decide the points to interpolate based on y
+        if y < vertices[1][1]:
+            border_colors[order[0]] = [vector_interp(vertices[0], vertices[1], vcolors[0][c], vcolors[1][c], y, 2) for c in range(3)]
+        else:
+            border_colors[order[0]] = [vector_interp(vertices[1], vertices[2], vcolors[1][c], vcolors[2][c], y, 2) for c in range(3)]           
+        border_colors[order[1]] = [vector_interp(vertices[0], vertices[2], vcolors[0][c], vcolors[2][c], y, 2) for c in range(3)]        
         
         # For each pixel in the x scanning range, find the final gouraud color from interpolation
-        xmin = current_edges[0]
-        xmax = current_edges[-1]
+        xmin, xmax = np.min(current_edges), np.max(current_edges)
         for x in range(xmin, xmax, 1):
-            gouraud_color = [0, 0, 0]
-            for i in range(3):
-                gouraud_color[i] = vector_interp([xmin, y], [xmax, y], border_colors[0][i], border_colors[1][i], x, 1)
+            gouraud_color = [vector_interp([xmin, y], [xmax, y], border_colors[0][c], border_colors[1][c], x, 1) for c in range(3)]
             updated_img[x][y] = gouraud_color
     return updated_img
 
 # Example usage
-#M = 1000
-#N = 1000
-#img = [[[0.99 for i in range(3)] for j in range(N)] for k in range(M)]
+M = 1000
+N = 1000
+img = np.full((M, N, 3), 0.99)
 #vertices = [[90, 52], [23, 66], [28, 38]]
 #vcolors = [[0.83, 0.93, 0.25], [0.33, 0.8, 0.61], [0.03, 0.73, 0.6]]
 
 # Randomize vertices and colors
-#vertices = []
-#vcolors = []
-#for i in range(3):
-#  vertices.append([random.randint(0, M - 1), random.randint(0, N - 1)])
-#  vcolors.append([random.randint(0, 99) / 100, random.randint(0, 99) / 100, random.randint(0, 99) / 100])print(vertices)print(vcolors)
+vertices = np.array([[random.randint(0, M - 1), random.randint(0, N - 1)] for i in range(3)])
+vcolors = np.array([[random.randint(0, 99) / 100 for i in range(3)] for j in range(3)])
 
 # Perform the gouraud shading function
-#img = g_shading(img, vertices, vcolors)
+img = g_shading(img, vertices, vcolors)
 
 # Show results
-#plt.imshow(img)
-#plt.show()
+plt.imshow(img)
+plt.show()
